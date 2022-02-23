db9e97b4d302ff70ea444663842065d2d31f34d5
Payam Siyahpoosh <Disalo@Payams-MacBook-Pro.local>
2022-02-23 14:34:12 +0100

    add files

---
+++ dummy_recommender.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import random
+
+movies = pd.read_csv('data/movies.csv')
+
+
+def recommend_random(samples):
+    user_movies = movies.sample(samples)
+    movie_ids = [random.randint(1,9741) for i in range(0,5) if random.randint(1,9741) not in user_movies]
+    recs = [movies['title'][i] for i in movie_ids if i in movies['movieId']]
+    return dict(zip(movie_ids,recs))
---
+++ main.py
@@ -0,0 +1,4 @@
+import dummy_recommender
+from dummy_recommender import recommend_random
+
+print(recommend_random(10))
