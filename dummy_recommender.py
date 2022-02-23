ff0e0a4cf79577bf664c57d42a788e47a458f690
Payam Siyahpoosh <Disalo@Payams-MacBook-Pro.local>
2022-02-23 15:01:09 +0100

    add files

--- dummy_recommender.py
+++ dummy_recommender.py
@@ -1,8 +1,11 @@
-import pandas as pd
 import random
+import pandas as pd
 
 movies = pd.read_csv('data/movies.csv')
 
+'''
+rocmmender
+'''
 
 def recommend_random(samples):
     user_movies = movies.sample(samples)
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import dummy_recommender
 from dummy_recommender import recommend_random
 
 print(recommend_random(10))
