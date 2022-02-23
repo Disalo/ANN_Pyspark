e14c4b56578b7de45b67b0777757840ed5e570ab
Payam Siyahpoosh <Disalo@Payams-MacBook-Pro.local>
2022-02-23 15:13:49 +0100

    add files

--- dummy_recommender.py
+++ dummy_recommender.py
@@ -10,5 +10,5 @@ rocmmender
 def recommend_random(samples):
     user_movies = movies.sample(samples)
     m_ids = [random.randint(1,90) for i in range(0,5) if random.randint(1,90) not in user_movies]
-    recs = [movies['title'][i] for i in movie_ids if i in movies['m_ids']]
+    recs = [movies['title'][i] for i in m_ids if i in movies['movieId']]
     return dict(zip(m_ids,recs))
